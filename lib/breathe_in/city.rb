2ead1c10b22d5759da232a2e8f06eb18702ca009
eric-an <erican@me.com>
2016-01-18 05:40:23 -0800

    define methods for City class

--- lib/breathe_in/city.rb
+++ lib/breathe_in/city.rb
@@ -1,8 +1,23 @@
 class BreatheIn::City
+  attr_accessor :city_name, :today_high, :today_index, :last_update_time, :last_update_value, :last_update_index
 
-#methods
-  # every city will be initialized with the various aspects of air quality
-  # design a method to create a new city
-  # design a method to add air quality attributes to the city
-  # create a class variable that will hold all the cities
+  @@cities = []
+
+  def initialize(city_hash)
+    city_hash.each { |key, value| self.send(("#{key}="), value) }
+    @@cities << self
+  end
+
+  def add_city_air_quality(air_quality_hash)
+    air_quality_hash.each { |key, value| self.send(("#{key}="), value) }
+    self
+  end
+
+  def self.cities
+    @@cities
+  end
+
+  def self.reset
+    @@cities.clear
+  end
 end
