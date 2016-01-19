daa953be30e37a53be9b64a634fd1e89ac83fa74
eric-an <erican@me.com>
2016-01-19 02:40:32 -0800

    add zipcode attribute to City class

--- lib/breathe_in/city.rb
+++ lib/breathe_in/city.rb
@@ -1,16 +1,15 @@
 class BreatheIn::City
-  attr_accessor :city_name, :today_high, :today_index, :last_update_time, :last_update_value, :last_update_index
+  attr_accessor :city_name, :zipcode, :today_high, :today_index, :last_update_time, :last_update_value, :last_update_index
 
   @@cities = []
 
-  def initialize(city_hash)
+  def initialize(city_hash={})
     city_hash.each { |key, value| self.send(("#{key}="), value) }
     @@cities << self
   end
 
   def add_city_air_quality(air_quality_hash)
     air_quality_hash.each { |key, value| self.send(("#{key}="), value) }
-    self
   end
 
   def self.cities
